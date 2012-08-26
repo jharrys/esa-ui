497614ef2f3fae8c800d70cd8507fb1546bc7861
John Harris on D91M31M1/Windows 7 <john.harris@imail.org>
2012-08-25 19:11:58 -0600

    Added my own tag library for logging within gsp.

---
+++ test/unit/esa/ui/LogTagLibTests.groovy
@@ -0,0 +1,17 @@
+package esa.ui
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.web.GroovyPageUnitTestMixin} for usage instructions
+ */
+@TestFor(LogTagLib)
+class LogTagLibTests {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
