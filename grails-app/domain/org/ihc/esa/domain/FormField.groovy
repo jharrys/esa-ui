e5564ef913035be758bed04fbf7f1907f728ba86
John Harris on D91M31M1/Windows 7 <john.harris@imail.org>
2012-08-28 20:13:29 -0600

    Modified domain class FormField.
    Added pageNumber and orderNumber and made sectionNumber not nullable

--- grails-app/domain/org/ihc/esa/domain/FormField.groovy
+++ grails-app/domain/org/ihc/esa/domain/FormField.groovy
@@ -1,60 +1,62 @@
 package org.ihc.esa.domain
 
 /***************************************************************************
-	Generated code by GenGroovyObjects [09-Aug-2012 20:45:44 -0600]
-	Copyright 2012 by Intermountain Healthcare
-***************************************************************************/
+ Generated code by GenGroovyObjects [09-Aug-2012 20:45:44 -0600]
+ Copyright 2012 by Intermountain Healthcare
+ ***************************************************************************/
 
-import java.util.Date
 class FormField {
-
-    BigDecimal sectionNumber
-    String question
-    String dataType
-    String multiSelect
-    Date dateCreated
-    String createdBy
-    Date lastUpdated
-    String updatedBy
+	
+	BigDecimal pageNumber
+	BigDecimal sectionNumber
+	BigDecimal orderNumber
+	String question
+	String dataType
+	String multiSelect
+	Date dateCreated
+	String createdBy
+	Date lastUpdated
+	String updatedBy
 	LookupList lookupList
-
-    static hasMany = [
-        responseToQuestion: QuestionResponse
-    ]
-
-    static belongsTo = [
-        form: Form
-    ]
-
-    static mapping = {
-
-        id generator:'sequence', params:[sequence:'FORM_FIELD_SEQ']
-        table 'FORM_FIELD'
-        version false
-
-        form column: 'FORM_ID'
-        sectionNumber column: 'SECTION_NUMBER'
-        question column: 'QUESTION'
-        dataType column: 'DATA_TYPE'
-        multiSelect column: 'MULTI_SELECT'
-        lookupList column: 'LOOKUP_LIST_ID'
-        dateCreated column: 'DATE_CREATED'
-        createdBy column: 'CREATED_BY'
-        lastUpdated column: 'LAST_UPDATED'
-        updatedBy column: 'UPDATED_BY'
-
-    }
-
-    static constraints = {
-
-        form nullable: false
-        sectionNumber nullable: true
-        question nullable: false
-        dataType nullable: false
-        multiSelect nullable: false
-        createdBy nullable: false
-        updatedBy nullable: false
-
-    }
-
+	
+	static hasMany = [
+		responseToQuestion: QuestionResponse
+	]
+	
+	static belongsTo = [
+		form: Form
+	]
+	
+	static mapping = {
+		
+		id generator:'sequence', params:[sequence:'FORM_FIELD_SEQ']
+		table 'FORM_FIELD'
+		version false
+		
+		form column: 'FORM_ID'
+		pageNumber column: 'PAGE_NUMBER'
+		sectionNumber column: 'SECTION_NUMBER'
+		orderNumber column: 'ORDER_NUMBER'
+		question column: 'QUESTION'
+		dataType column: 'DATA_TYPE'
+		multiSelect column: 'MULTI_SELECT'
+		lookupList column: 'LOOKUP_LIST_ID'
+		dateCreated column: 'DATE_CREATED'
+		createdBy column: 'CREATED_BY'
+		lastUpdated column: 'LAST_UPDATED'
+		updatedBy column: 'UPDATED_BY'
+	}
+	
+	static constraints = {
+		
+		form nullable: false
+		pageNumber nullable: true
+		sectionNumber nullable: false
+		orderNumber nullable: true
+		question nullable: false
+		dataType nullable: false
+		multiSelect nullable: false
+		createdBy nullable: false
+		updatedBy nullable: false
+	}
 }
