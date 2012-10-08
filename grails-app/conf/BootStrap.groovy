1eebfcd626002d65b3fd7b674f6da81d023c5d80
John Harris on D91M31M1/Cygwin <john.harris@imail.org>
2012-10-07 21:01:24 -0600

    refactored bootstrap to load dev environment from esa-content submodule

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,8 +1,11 @@
+import java.sql.Statement
 import org.ihc.esa.*
+import org.h2.tools.*
 
 class BootStrap
 {
-	
+	def grailsApplication
+	def sessionFactory
 	def init =
 	{ servletContext ->
 		environments
@@ -45,28 +48,99 @@ class BootStrap
 				assert EsaUser.count() == 9
 				assert EsaRole.count() == 3
 				assert EsaUserEsaRole.count() == 9
+				/*-----------------------------------------------------------*/
+				def session = null
+				def connection = null
+				if (grailsApplication.getFlatConfig().get("dataSource.url").startsWith("jdbc:h2"))
+				{
+					//if we are running with the internal H2 database, then execute scripts with H2 specific queries.
+					session = sessionFactory.getCurrentSession()
+					connection = session.connection()
+				}
 				
-				/***********************************************************/
-				Form exception = new Form(id:1, name:"exception", description:"exception form", createdBy:"lpjharri", updatedBy:"lpjharri")
-				exception.save(flush: true, failOnError: true)
+				if (connection != null)
+				{
+					Statement s = connection.createStatement()
+					s.execute("CREATE ALIAS TO_TIMESTAMP AS \$\$ " +
+									"java.sql.Date toTimestamp(String one, String two) " +
+									"{ return new java.sql.Date((new java.util.Date()).getTime()); } \$\$")
+					
+					Reader reader = null
+					
+					reader = new BufferedReader(new FileReader("../../form_seed_data.sql"))
+					RunScript.execute(connection, reader)
+					reader.close()
+					assert Form.count() == 4
+					
+					reader = new BufferedReader(new FileReader("../../party_seed_data.sql"))
+					RunScript.execute(connection, reader)
+					reader.close()
+					assert Party.count() == 42
+					
+					reader = new BufferedReader(new FileReader("../../configuration_parameter_seed_data.sql"))
+					RunScript.execute(connection, reader)
+					reader.close()
+					assert ConfigurationParameter.count() == 1
+					
+					reader = new BufferedReader(new FileReader("../../lookup_list_seed_data.sql"))
+					RunScript.execute(connection, reader)
+					reader.close()
+					assert LookupList.count() == 14
+					
+					reader = new BufferedReader(new FileReader("../../lookup_element_seed_data.sql"))
+					RunScript.execute(connection, reader)
+					reader.close()
+					assert LookupElement.count() == 29
+					
+					reader = new BufferedReader(new FileReader("../../form_field_seed_data.sql"))
+					RunScript.execute(connection, reader)
+					reader.close()
+					assert FormField.count() == 44
+					
+					reader = new BufferedReader(new FileReader("../../item_seed_data_from_eisa_excel.sql"))
+					RunScript.execute(connection, reader)
+					reader.close()
+					assert Item.count() == 341
+				}
+			}	// end-development
+			test {
+				def adminRole = new EsaRole(authority: 'ROLE_ESA_ADMIN').save(flush: true)
+				def userRole = new EsaRole(authority: 'ROLE_ESA_USER').save(flush: true)
+				def bogusRole = new EsaRole(authority: 'ROLE_ESA_BOGUS').save(flush: true)
 				
-				assert Form.count() == 1
+				def manager = new EsaUser(username: 'manager', email_address: 'john.harris@imail.org', enabled: true, password: 'esa')
+				def user = new EsaUser(username: 'lpjharri', email_address: 'john.harris@imail.org', enabled: true, password: 'esa')
+				def user1 = new EsaUser(username: 'tssimpso', email_address: 'stuart.simpson@imail.org', enabled: true, password: 'esa')
+				def user2 = new EsaUser(username: 'gknarra', email_address: 'gopal.narra@imail.org', enabled: true, password: 'esa')
+				def user3 = new EsaUser(username: 'ldsgrove', email_address: 'sara.curry@imail.org', enabled: true, password: 'esa')
+				def user4 = new EsaUser(username: 'tshipley', email_address: 'tim.shipley@imail.org', enabled: true, password: 'esa')
+				def user5 = new EsaUser(username: 'lbuchanan', email_address: 'lonnie.buchanan@imail.org', enabled: true, password: 'esa')
+				def user6 = new EsaUser(username: 'eisa', email_address: 'eisa-repository-notify@imail.org', enabled: true, password: 'esa')
+				def user7 = new EsaUser(username: 'donottrust', email_address: 'john.harris@ihc.com', enabled: false, password: 'esa')
+				manager.save(flush: true)
+				user.save(flush: true)
+				user1.save(flush: true)
+				user2.save(flush: true)
+				user3.save(flush: true)
+				user4.save(flush: true)
+				user5.save(flush: true)
+				user6.save(flush: true)
+				user7.save(flush: true)
 				
-				/***********************************************************/
-				FormField ff1 = new FormField(id: 1,form: exception,searchListing:"Y",pageNumber: 1,sectionNumber: 1,orderNumber: 1,required: 'N',internalOnly: 'N',question: 'Title Of Exception',dataType: 'Title',multiSelect: 'N',cssClass: 'titleHeader',createdBy: 'bootstrap',updatedBy: 'bootstrap')
-				FormField ff2 = new FormField(id: 2,form: exception,searchListing:"Y",pageNumber: 1,sectionNumber: 1,orderNumber: 2,required: 'N',internalOnly: 'N',question: 'Introduction',dataType: 'SectionHeader',multiSelect: 'N',cssClass: 'sectionHeader',createdBy: 'bootstrap',updatedBy: 'bootstrap')
-				FormField ff3 = new FormField(id: 3,form: exception,searchListing:"Y",pageNumber: 1,sectionNumber: 1,orderNumber: 3,required: 'N',internalOnly: 'N',question: '<p>EISA APPLICATION FOR EXCEPTION: CLINICAL AND BUSINESS APPLICATIONS</p> <p>This exception is a technical approval from Enterprise IS Architecture and is not an indication that resources have been committed and approved to implement, install, or support the described application or system.</p><p>Instructions:</p><p>This exception process is required to purchase and/or install a Business Application or Technology for which there is already an established standard. If a standard does not already exist, do not use this process unless directed by Enterprise IS Architecture. Contact Enterprise IS Architecture (mail: Enterprise IS Architecture) for help.  This exception process requires business level approvals before purchase or installation can begin. Because an enterprise wide standard already exists, you and information Services will likely incur higher support costs for duplicate or competing applications.  The form must be completed and returned to the EISA Team prior to the purchase or implementation of this exception.</p>',dataType: 'Title',multiSelect: 'N',cssClass: 'titleHeader',createdBy: 'bootstrap',updatedBy: 'bootstrap')
-				FormField ff4 = new FormField(id: 4,form: exception,searchListing:"Y",pageNumber: 1,sectionNumber: 1,orderNumber: 4,required: 'Y',internalOnly: 'N',question: 'Submission Date',dataType: 'DATE_VALUE',multiSelect: 'N',cssClass: 'textField',createdBy: 'bootstrap',updatedBy: 'bootstrap')
-				FormField ff5 = new FormField(id: 5,form: exception,searchListing:"Y",pageNumber: 1,sectionNumber: 1,orderNumber: 5,required: 'Y',internalOnly: 'N',question: 'Please provide the justification for the exemption from the standard: (Document attachments will be made available after this wizard completes)',dataType: 'STRING_VALUE',multiSelect: 'N',cssClass: 'textArea',formInputType: 'textArea',createdBy: 'bootstrap',updatedBy: 'bootstrap')
+				EsaUserEsaRole.create manager, adminRole, true
+				EsaUserEsaRole.create user, userRole, true
+				EsaUserEsaRole.create user1, userRole, true
+				EsaUserEsaRole.create user2, userRole, true
+				EsaUserEsaRole.create user3, userRole, true
+				EsaUserEsaRole.create user4, userRole, true
+				EsaUserEsaRole.create user5, userRole, true
+				EsaUserEsaRole.create user6, bogusRole, true
+				EsaUserEsaRole.create user7, bogusRole, true
 				
-				ff1.save(flush: true, failOnError: true)
-				ff2.save(flush: true, failOnError: true)
-				ff3.save(flush: true, failOnError: true)
-				ff4.save(flush: true, failOnError: true)
-				ff5.save(flush: true, failOnError: true)
-				
-				assert FormField.count() == 5
-			}
+				assert EsaUser.count() == 9
+				assert EsaRole.count() == 3
+				assert EsaUserEsaRole.count() == 9
+			} // end-test
 		}
 	}
 	
