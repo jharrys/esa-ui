418e5967216fa971c64e49b2eb7c783db0b88531
John Harris on D91M31M1/Windows 7 <john.harris@imail.org>
2012-10-23 00:35:50 -0600

    modified build hook to write commit number only when the 'release' tag is tied to HEAD

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -1,3 +1,11 @@
 eventCompileStart = { msg ->
-  new File("grails-app/views/_git.gsp").text = ("git rev-parse HEAD".execute().text)
+	final String tagName = "release"
+	String headCommit = ("git rev-parse HEAD".execute().text)
+	String cmd = "git describe --tags --match " + tagName + " HEAD"
+	String headTag = (cmd.execute().text.trim())
+	
+	if (tagName.equals(headTag)) {
+		println "found tag named " + tagName + " - writing commit " + headCommit + " to _git.gsp"
+		new File("grails-app/views/_git.gsp").text = headCommit
+	} 
 }
