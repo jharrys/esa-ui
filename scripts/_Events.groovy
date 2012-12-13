1d153b826003a390ac9b6404544c472e46a16b9b
John Harris on D91M31M1/Windows 7 <john.harris@imail.org>
2012-12-13 13:16:20 -0700

    Fixed script to create _git.gsp so it doesn't break the layout during dev. builds.

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -1,11 +1,16 @@
 eventCompileStart = { msg ->
 	final String tagName = "release"
+	File gitFile = new File("grails-app/views/_git.gsp")
 	String headCommit = ("git rev-parse HEAD".execute().text)
 	String cmd = "git describe --tags --match " + tagName + " HEAD"
 	String headTag = (cmd.execute().text.trim())
-	
+
 	if (tagName.equals(headTag)) {
 		println "found tag named " + tagName + " - writing commit " + headCommit + " to _git.gsp"
-		new File("grails-app/views/_git.gsp").text = headCommit
-	} 
+		gitFile.text = headCommit
+	} else {
+		if (!gitFile.exists()) {
+			gitFile.text = 'in development mode'
+		}
+	}
 }
