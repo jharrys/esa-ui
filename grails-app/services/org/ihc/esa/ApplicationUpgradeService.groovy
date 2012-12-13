7c56fbdd19f2a294cdf4c42cd140ff7520874fe2
John Harris on D91M31M1/Windows 7 <john.harris@imail.org>
2012-12-13 13:18:54 -0700

    Added ApplicationUpgrade service.
    It is set to work with the jmx grails plugin, to be exposed through jmx at some point.

---
+++ grails-app/services/org/ihc/esa/ApplicationUpgradeService.groovy
@@ -0,0 +1,33 @@
+package org.ihc.esa
+
+class ApplicationUpgradeService
+{
+	static transactional = false
+
+	static expose = ['jmx']
+
+	def grailsApplication
+
+	ApplicationUpgradeService()
+	{
+		super()
+	}
+
+	void updateAllDomainClassIndices()
+	{
+		isSearchable =
+		{ domainClass ->
+			ClassPropertyFetcher.forClass( domainClass ).getStaticPropertyValue( "search", Closure ) || AnnotationUtils.isAnnotationDeclaredLocally( Indexed, domainClass )
+		}
+
+		grailsApplication.domainClasses*.clazz.findAll( isSearchable ).each
+		{
+
+			println "Creating Lucene index for entity [${it.name}]...."
+
+			it.search().createIndexAndWait()
+		}
+
+		println "Done."
+	}
+}
