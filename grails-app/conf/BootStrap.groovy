b49c70365091183bf480ea2c26a33b13a84152c9
John Harris on D91M31M1/Windows 7 <john.harris@imail.org>
2013-01-16 09:48:35 -0700

    Upped the minimumDatabaseVersion to 1.5.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -6,7 +6,7 @@ import org.ihc.esa.*
 class BootStrap
 {
 	// compatibility matrix: [app 0.5.0-0.8.1 with db 1.1], [app 0.9.0 with db 1.2], [app 1.0 with db 1.3]
-	private final String minimumDatabaseVersion = "1.3"
+	private final String minimumDatabaseVersion = "1.5"
 	private final String minimumApplicationVersion = "1.0.0"
 	
 	def grailsApplication
