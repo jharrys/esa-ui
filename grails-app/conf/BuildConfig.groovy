fb24a0429818d1c61803ae1f34de1e32a9c2b54d
John Harris on D91M31M1/Windows 7 <john.harris@imail.org>
2013-01-16 18:46:07 -0700

    Removal of code from production closure.
    Only caught now in production because I turned validation on.
    Also bumped up version in application.properties.
    Changed BuildConfig to add version number to war file.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -5,7 +5,7 @@ grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 grails.project.target.level = 1.6
 grails.project.source.level = 1.6
-grails.project.war.file = "esa-ui.war"
+//grails.project.war.file = "esa-ui.war"
 //grails.war.resources =
 //{ stagingDir, args ->
 //	copy(file: "grails-app/conf/log4j.groovy",
