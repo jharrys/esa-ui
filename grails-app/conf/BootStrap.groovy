c13c94a379dbf000e1e34ad484f5b69cb8281d44
John Harris on D91M31M1/Windows 7 <john.harris@imail.org>
2013-02-25 15:59:39 -0700

    Rolled app version to 1.1.2.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -5,9 +5,9 @@ import org.ihc.esa.*
 
 class BootStrap
 {
-	// compatibility matrix: [app 0.5.0-0.8.1 with db 1.1], [app 0.9.0 with db 1.2], [app 1.0 with db 1.3], [app 1.1 with db 1.5], [app 1.1.1 with db 1.6]
+	// compatibility matrix: [app 0.5.0-0.8.1 with db 1.1], [app 0.9.0 with db 1.2], [app 1.0 with db 1.3], [app 1.1 with db 1.5], [app 1.1.1-1.1.2 with db 1.6]
 	private final String minimumDatabaseVersion = "1.6"
-	private final String minimumApplicationVersion = "1.1.1"
+	private final String minimumApplicationVersion = "1.1.2"
 	
 	def grailsApplication
 	def sessionFactory
