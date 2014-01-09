5397206b3a15c1d95f73b19c93417664fb82b5b7
Johnnie Harris <johnnie.harris@imail.org>
2014-01-09 01:07:36 -0700

    Upgraded several plugins.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -29,8 +29,8 @@ grails.project.dependency.resolution =
 	}
 	
 	dependencies
-	{ 
-		compile "org.jadira.usertype:usertype.jodatime:1.9" 
+	{
+		compile "org.jadira.usertype:usertype.jodatime:1.9"
 		test "org.spockframework:spock-grails-support:0.7-groovy-2.0"
 	}
 	
@@ -40,10 +40,10 @@ grails.project.dependency.resolution =
 		compile ":hibernate-search:0.7"
 		compile ":joda-time:1.4"
 		compile ":fields:1.3"
-		compile ":jquery:1.8.3"
-		compile ":resources:1.2.RC2"
+		compile ":jquery:1.10.2.2"
+		compile ":resources:1.2.1"
 		compile ":famfamfam:1.0.1"
-		compile ":jquery-ui:1.8.24"
+		compile ":jquery-ui:1.10.3"
 		compile ":mail:1.0.1"
 		compile ":spring-security-core:1.2.7.3"
 		compile ":spring-security-eventlog:0.2"
@@ -51,17 +51,15 @@ grails.project.dependency.resolution =
 		compile ":spring-security-ui:0.2"
 		compile ":jasper:1.6.1"
 		compile ":webxml:1.4.1"
-		compile ":jmx:0.8"
+		compile ":jmx:0.9"
 		compile ":twitter-bootstrap:2.2.2"
-		compile ":external-config-reload:1.2.2"
+		compile ":external-config-reload:1.4.1"
 		
 		runtime ":cache-headers:1.1.5"
 		runtime ":zipped-resources:1.0"
 		runtime ":cached-resources:1.0"
 		
-		test (":spock:0.7") {
-			exclude "spock-grails-support"
-		}
+		test (":spock:0.7") { exclude "spock-grails-support" }
 		
 		build ":tomcat:$grailsVersion"
 	}
